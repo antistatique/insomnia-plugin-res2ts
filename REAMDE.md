20237e0868d5b0a04bb0746bef12b2f0eb8710ca
Yann Gouffon <github@yago-art.ch>
2021-06-01 11:42:51 +0200

    🎉 add first version

---
+++ REAMDE.md
@@ -0,0 +1,16 @@
+# Insomnia - Response to Typescript Types
+
+Add a Request Action to generate TypeScript *types based on the response data.
+
+![screenshot](./screenshot.png)
+
+\**the root type will be based on the filename + capitalisation*
+
+## Install
+
+1. Go to `Insomnia/Preferences` and choose the *Plugins* tab
+2. Enter `insomnia-plugin-res2ts` and click on `Install Plugin`
+3. That's it, enjoy!
+
+## Credits
+- [json-ts](https://github.com/shakyshane/json-ts)
---
+++ index.js
@@ -0,0 +1,36 @@
+const { json2ts } = require('json-ts');
+const fs = require('fs')
+
+module.exports.requestActions = [
+  {
+    label: 'Generate TypeScript Types',
+    action: async (context, data) => {
+      const { request } = data;
+      console.log('request', request);
+      const response = await context.network.sendRequest(request);
+      const body = fs.readFileSync(response.bodyPath);
+      const interfaces = json2ts(body, { prefix: '', rootName: 'RootObject' });
+      
+      context.app.showSaveDialog().then((filePath) => {
+        if (filePath !== null) {
+          const fileName = filePath.split('/').pop().split('.')[0];
+          const rootName = fileName
+            .split('-')
+            .map(s => s.charAt(0).toUpperCase() + s.slice(1))
+            .join('')
+            .replace(/\W/g, '');
+          const body = interfaces
+            .replace(/\{/gm, '= {')
+            .replace(/\}/gm, "};\n")
+            .replace(/\s\s\s\s/gm, "\n ")
+            .replace('interface RootObject', `export type ${rootName}`)
+            .replace(/interface/gm, 'type');
+
+          fs.writeFile(filePath, body, (err) => {
+            console.log('err', err);
+          });
+        }
+      });
+    },
+  },
+];
