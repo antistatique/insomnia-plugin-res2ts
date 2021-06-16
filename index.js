b3ee2de0d4600f82e87e516b9f568d3d4a20fcd7
Yann Gouffon <github@yago-art.ch>
2021-06-16 17:31:44 +0200

    ♻️ refactor to quicktype

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { json2ts } = require('json-ts');
+const {
+  quicktype,
+  jsonInputForTargetLanguage,
+  InputData,
+} = require("quicktype-core");
 const fs = require('fs')
 
 module.exports.requestActions = [
@@ -8,9 +12,8 @@ module.exports.requestActions = [
       const { request } = data;
       const response = await context.network.sendRequest(request);
       const body = fs.readFileSync(response.bodyPath);
-      const interfaces = json2ts(body, { prefix: '', rootName: 'RootObject' });
-      
-      context.app.showSaveDialog().then((filePath) => {
+            
+      context.app.showSaveDialog().then(async (filePath) => {
         if (filePath !== null) {
           const fileName = filePath.split('/').pop().split('.')[0];
           const rootName = fileName
@@ -18,14 +21,34 @@ module.exports.requestActions = [
             .map(s => s.charAt(0).toUpperCase() + s.slice(1))
             .join('')
             .replace(/\W/g, '');
-          const body = interfaces
+
+          const jsonInput = jsonInputForTargetLanguage('ts');
+          await jsonInput.addSource({
+            name: rootName,
+            samples: [body],
+
+          });
+
+          const inputData = new InputData();
+          inputData.addInput(jsonInput);
+
+          const interfaces = await quicktype({
+            inputData,
+            lang: 'ts',
+            rendererOptions: {
+              'just-types': true,
+              'runtime-typecheck': false
+            }
+          });
+
+          console.log('interfaces', interfaces.lines.join('\n'));
+
+          const content = interfaces.lines.join('\n')
             .replace(/\{/gm, '= {')
-            .replace(/\}/gm, "};\n")
-            .replace(/\s\s\s\s/gm, "\n ")
-            .replace('interface RootObject', `export type ${rootName}`)
+            .replace(/\};/gm, "};\n")
             .replace(/interface/gm, 'type');
 
-          fs.writeFile(filePath, body, (err) => {
+          fs.writeFile(filePath, content, (err) => {
             console.log('err', err);
           });
         }
